dd7ea40fb8c8b1e148e84915d4e931bd603530c4
yamagen0829 <yamagen3745@gmail.com>
2024-04-06 21:38:15 +0900

    kadai_009を再提出

--- kadai_009/horiday.js
+++ kadai_009/horiday.js
@@ -1,13 +1,14 @@
 //休日の種類 
-var holiDays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+let holiDays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
 
 // for文
-for (var i = 0; i <holiDays.length; i ++){
+for (let i = 0; i <holiDays.length; i ++){
   console.log(holiDays[i]);
 }
 
 // while文
-while (holiDays[0] !== '正月'){
-   holiDays = shuffleArray(holiDays);
+ let i = 0;
+ while (i < holiDays.length){
+    console.log(holiDays[i]);
+    i ++;
 }
-console.log(holiDays);
