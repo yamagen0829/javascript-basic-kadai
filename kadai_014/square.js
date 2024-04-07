1033532b283b2f8497bade20fbb11f7d726acd70
yamagen0829 <yamagen3745@gmail.com>
2024-04-07 22:06:23 +0900

    kadai_014を追加

---
+++ kadai_014/square.js
@@ -0,0 +1,15 @@
+// 教材通りの記述
+//与えられた引数num乗算し、その値を出力する関数を定義する
+const addnum = (num) => {
+  console.log(num*10);
+}
+// 関数を呼び出し、引数として数値を渡す
+addnum(10);
+
+// 検索でヒットした記述
+const square = (num) => {
+    const result = num ** 2;
+    console.log(result);
+}
+//100が出力される  
+  square(10);    
